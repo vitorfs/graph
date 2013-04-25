f9d24ce39cf11501167304d985a41f4ee7db3e17
Vitor Freitas <vitorfs@gmail.com>
2013-04-25 16:42:28 -0300

    Creating first methods for adjacency list

--- adjacency_list.c
+++ adjacency_list.c
@@ -18,6 +18,22 @@
 
 #include "adjacency_list.h"
 
+void empty_adjlist(Adj_List* g) {
+  g = NULL;
+}
+
+void exist_arc_adjlist(Adj_List* g, int v1, int v2) {
+
+}
+
+void insert_arc_adjlist(Adj_List* g, int v1, int v2, int weight) {
+  
+}
+
+void remove_arc_adjlist(Adj_List* g, int v1, int v2) {
+
+}
+
 int main(int argc, char* argv[]) {
   return 0;
 }
