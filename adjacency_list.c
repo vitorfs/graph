25c6a11fa16e19fe7d5bfdb28f8dcb509c1e89bb
Vitor Freitas <vitorfs@gmail.com>
2013-04-26 23:34:15 -0300

    Added methods to adjacency_list.c

--- adjacency_list.c
+++ adjacency_list.c
@@ -34,6 +34,22 @@ void remove_arc_adjlist(Adj_List* g, int v1, int v2) {
 
 }
 
+void remove_arc_adjlist(Adj_List* g, int v1, int v2) {
+
+}
+
+void free_adjlist(Adj_List* g) {
+
+}
+
+void print_adjlist(Adj_List* g) {
+
+}
+
+void remove_min_arc(Adj_List* g) {
+
+}
+
 int main(int argc, char* argv[]) {
   return 0;
 }
